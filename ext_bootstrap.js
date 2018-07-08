c954f9b06cd4550b624774bf82391f0f4d1616f3
Mark Hammond <mhammond@skippinet.com.au>
2018-07-08 14:07:00 +1000

    Remove our item from the tools menu

--- ext_bootstrap.js
+++ ext_bootstrap.js
@@ -40,55 +40,6 @@ function debug(...args) {
   }
 }
 
-// Utilities to initialize the addon...
-function loadIntoWindow(window) {
-  if (!window)
-    return;
-  let wintype = window.document.documentElement.getAttribute('windowtype');
-  if (wintype != "navigator:browser") {
-    log("not installing aboutsync extension into window of type " + wintype);
-    return;
-  }
-  // Add persistent UI elements to the "Tools" ment.
-  let menuItem = window.document.createElement("menuitem");
-  menuItem.setAttribute("id", "aboutsync-menuitem");
-  menuItem.setAttribute("label", "About Sync");
-  menuItem.addEventListener("command", function(event) {
-    let win = event.target.ownerDocument.defaultView;
-    let tab = win.gBrowser.addTab("about:sync");
-    win.gBrowser.selectedTab = tab;
-  }, true);
-  let menu = window.document.getElementById("menu_ToolsPopup");
-  if (!menu) {
-    // might be a popup or similar.
-    log("not installing aboutsync extension into browser window as there is no Tools menu");
-  }
-  menu.appendChild(menuItem);
-  debug("installing aboutsync into new window");
-}
-
-function unloadFromWindow(window) {
-  if (!window)
-    return;
-  window.document.getElementById("aboutsync-menuitem").remove();
-  // Remove any persistent UI elements
-  // Perform any other cleanup
-}
-
-let windowListener = {
-  onOpenWindow: function(aWindow) {
-    // Wait for the window to finish loading
-    let domWindow = aWindow.QueryInterface(Ci.nsIInterfaceRequestor).getInterface(Ci.nsIDOMWindowInternal || Ci.nsIDOMWindow);
-    domWindow.addEventListener("load", function onLoad() {
-      domWindow.removeEventListener("load", onLoad, false);
-      loadIntoWindow(domWindow);
-    }, false);
-  },
-
-  onCloseWindow: function(aWindow) {},
-  onWindowTitleChange: function(aWindow, aTitle) {}
-};
-
 const ENGINE_NAMES = ["addons", "bookmarks", "clients", "forms", "history",
                       "passwords", "prefs", "tabs"];
 
@@ -193,16 +144,6 @@ function startup(data, reason) {
     Services.obs.addObserver(syncStatusObserver, topic, false);
   }
 
-  // Load into any existing windows
-  let windows = Services.wm.getEnumerator("navigator:browser");
-  while (windows.hasMoreElements()) {
-    let domWindow = windows.getNext().QueryInterface(Ci.nsIDOMWindow);
-    loadIntoWindow(domWindow);
-  }
-
-  // Load into any new windows
-  Services.wm.addListener(windowListener);
-
   // for some reason we can't use chrome://aboutsync at the top-level of
   // this module, but only after startup is called.
   const { Config } = Cu.import("chrome://aboutsync/content/config.js", {});
@@ -223,19 +164,6 @@ function shutdown(data, reason) {
 
   let wm = Cc["@mozilla.org/appshell/window-mediator;1"].getService(Ci.nsIWindowMediator);
 
-  // Stop listening for new windows
-  wm.removeListener(windowListener);
-
-  // Unload from any existing windows
-  let windows = wm.getEnumerator("navigator:browser");
-  while (windows.hasMoreElements()) {
-    let domWindow = windows.getNext().QueryInterface(Ci.nsIDOMWindow);
-    try {
-      unloadFromWindow(domWindow);
-    } catch (ex) {
-      log("Failed to reset window: " + ex + "\n" + ex.stack);
-    }
-  }
   Services.prefs.removeObserver(PREF_VERBOSE, prefObserver);
 
   for (let topic of SYNC_STATUS_TOPICS) {
